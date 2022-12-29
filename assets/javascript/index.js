d4776b7b831a97e182b1a96f16722945b3fa1acf
Craig <panamaworldwideinvestments@gmail.com>
2022-12-29 10:03:23 +0000

    added js section

---
+++ assets/javascript/index.js
@@ -0,0 +1,26 @@
+console.log('Hello-World');
+
+alert('Hello everyone!');
+
+let cars = ["Volvo", "Jaguar", "Porsche", "Toyota", "Mercedes"];
+
+console.log(cars[1]);
+
+let car = {
+    brand:"Toyota",
+    compnents:["wheels", "window","engine", "windscreen"],
+    color:"white",
+    age:10,
+    isInsured:false
+
+
+
+};
+
+console.log(car.compnents[3]);
+
+function add(a,b ){
+    return a + b;
+
+}
+console.log(add(7, 5));
